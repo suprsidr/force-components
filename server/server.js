f069732b256b94be20462bdc16ea7cbc3007e961
suprsidr <suprsidr@gmail.com>
2016-02-18 06:28:03 -0600

    prep for TFS checkout/in

--- server/server.js
+++ server/server.js
@@ -3,6 +3,18 @@ var path = require('path');
 var fs = require('fs');
 var app = express();
 
+/* TFS setup */
+var tfs = require('win-tfs'),
+    basePath = path.resolve('C:/xampp/htdocs/StaticCMSContent-dev/ForceRc/snippets/homepage/'),
+    exe = tfs.findVisualStudioPath() + 'tf.exe';
+
+var paths = [
+  path.join(basePath, 'home-glamour-forcerc.html')
+];
+
+var params = ['/login:tfsusername@domain,PASSWORD', '/comment:Updated from nodejs', '/noprompt'];
+/* End TFS */
+
 app.use(express.static(path.join(__dirname, './../')));
 
 app.get('/', function(req,res) {
@@ -10,7 +22,7 @@ app.get('/', function(req,res) {
 });
 
 app.get('/slides', function(req,res) {
-  res.sendFile('C:\\xampp\\htdocs\\StaticCMSContent-dev\\ForceRc\\snippets\\homepage\\home-glamour-forcerc.html');
+  res.sendFile(path.join(basePath, 'home-glamour-forcerc.html'));
 });
 
 app.listen(8080, function() {
