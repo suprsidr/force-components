9aabb8b9d9ff3d97bbf31f0bd857f63f77b3e48c
suprsidr <suprsidr@gmail.com>
2016-03-06 13:39:48 -0600

    Foundation menu

--- components/Main.js
+++ components/Main.js
@@ -5,11 +5,23 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <ul role="nav">
-          <li><IndexLink to="/">Home</IndexLink></li>
-          <li><Link to="/slides" activeStyle={{ color: 'red' }}>Homepage Slides</Link></li>
-          <li><Link to="/merch" activeStyle={{ color: 'red' }}>MerchPack</Link></li>
-        </ul>
+        <div className="top-bar" role="nav">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className="menu-text">ForceRC Markup Generator</li>
+              <li><IndexLink to="/">Home</IndexLink></li>
+              <li><Link to="/slides" activeStyle={{ color: 'red' }}>Homepage Slides</Link></li>
+              <li><Link to="/merch" activeStyle={{ color: 'red' }}>MerchPack</Link></li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <ul className="menu">
+              <li><input type="search" placeholder="Search" /></li>
+              <li><button type="button" className="button">Search</button></li>
+            </ul>
+          </div>
+        </div>
+        <p>&nbsp;</p>
         <div>
           {this.props.children}
         </div>
