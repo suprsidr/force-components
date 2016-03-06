7e810a5bec4d05a2aa22a9da355227d473f06ea8
suprsidr <suprsidr@gmail.com>
2016-03-06 08:29:57 -0600

    update textarea on state.elements update

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-
+import ReactDOMServer from 'react-dom/server';
 import EditableAnchor from './elements/EditableAnchor';
+import html from 'html';
 
 class Home extends Component{
   constructor(props) {
     super(props);
+    this.renderingOutput = false,
     this.state = {
       elements: []
     };
@@ -28,13 +30,33 @@ class Home extends Component{
       }, elements);
       el[last] = obj[key];
     });
-    this.updateState(elements);
+    this.updateState(elements, () => {
+      const ta = this.refs.textarea;
+      ta.value = this.setTextAreaValue();
+    });
   }
   onChange(e) {
     let el = document.createElement('div');
     el.innerHTML = e.target.value;
-    //console.log(Array.from(el.children));
+    //console.log('onChange');
     this.updateState(Array.from(el.children).map((e, i) => this.getDomMap(e, i)));
+  }
+  setTextAreaValue() {
+    this.renderingOutput = true;
+    var result = html.prettyPrint(
+      ReactDOMServer.renderToStaticMarkup(
+        React.createElement(
+          'junk',
+          {},
+          this.state.elements.map((el) => this.createElement(el))
+        )
+      )
+      .replace(/^<junk>/, '')
+      .replace(/<\/junk>$/, '')
+    );
+    this.renderingOutput = false;
+    // preserve ampersands in links & src
+    return result.replace(/&amp;/g, '&');
   }
   getDomMap(el, id) {
     return {
@@ -48,7 +70,7 @@ class Home extends Component{
         }, {_id: id, key: id}),
       children: Array.from(el.children).map((child, i) => this.getDomMap(child, `${id}_children_${i}`)),
       classList: Array.from(el.classList),
-      tag: el.tagName,
+      tag: el.tagName.toLowerCase(),
       textContent: Array.from(el.childNodes).reduce((prev, next) => {
         if(next.nodeType === 3) {
           prev += next.textContent.replace(/(\r\n|\n|\r|\t)/gm,'').replace(/\s{2,}/g, ' ');
@@ -58,21 +80,27 @@ class Home extends Component{
     }
   }
   renderChildren(tag) {
-    return tag.toLowerCase() !== 'img';
+    return tag !== 'img';
   }
   renderEditable(tag) {
-    return tag.toLowerCase() === 'a' ? EditableAnchor : tag;
+    return tag === 'a' && !this.renderingOutput ? EditableAnchor : tag;
   }
   createElement(el) {
     el.attributes.className = el.classList.length ? el.classList.join(' ') : null;
-    el.attributes.updateState = (obj) => this.updateStateFromChild(obj);
+    el.attributes.updateState = !this.renderingOutput ? (obj) => this.updateStateFromChild(obj) : null;
     var children =  el.children.length > 0 ? el.children.map((child) => this.createElement(child)) : el.textContent;
     return React.createElement(this.renderEditable(el.tag), el.attributes, this.renderChildren(el.tag) ? children : undefined);
+  }
+  // we don't want the toolbar in our formatted output.
+  createCleanElement(el) {
+    el.attributes.className = el.classList.length ? el.classList.join(' ') : null;
+    var children =  el.children.length > 0 ? el.children.map((child) => this.createElement2(child)) : el.textContent;
+    return React.createElement(el.tag, el.attributes, this.renderChildren(el.tag) ? children : undefined);
   }
   render() {
     return (
       <div>
-        <textarea style={{display: 'block', width: '100%', maxWidth: '960px', height: '400px', margin: '0 auto'}} onChange={(e) => this.onChange(e)}/>
+        <textarea ref="textarea" style={{display: 'block', width: '100%', maxWidth: '960px', height: '400px', margin: '0 auto'}} onChange={(e) => this.onChange(e)}/>
         <div className="main_content homepage">
           {this.state.elements.map((el) => this.createElement(el))}
         </div>
